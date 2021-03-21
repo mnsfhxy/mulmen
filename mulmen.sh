392649873ca5040a5247caa3727aafed9eaa1653
mnsfhxy <mnsfhxy@163.com>
2021-03-21 15:26:10 +0800

    ..

--- mulmen.sh
+++ mulmen.sh
@@ -4,7 +4,7 @@ GREEN='\E[1;32m'
 PINK='\E[1;35m'     
 RES='\E[0m'          
 PID_PATH='/var/mentohust.pid' #mentohust.pid的路径
-CONF_PATH='/etc/mulmen.conf' #配置文件路径
+CONF_PATH='./mulmen.conf' #配置文件路径
 MENTOHUST_STATU=$(ps -ef |grep -w mentohust|grep -v grep|wc -l)
 if [ -f $CONF_PATH ]
 then
@@ -13,18 +13,18 @@ else
 echo -e ${RED}${CONF_PATH}配置文件不存在!!${RES}
 exit
 fi
-echo -e ${RED}${RES}
+
 if [ ${MENTOHUST_STATU} -gt 0 ]
 then
 echo -e "${RED}检测到已开启mentohust${RES}"
-echo $(ps -ef |grep -w mentohust|grep -v)
+#echo $(ps -ef |grep -w mentohust|grep -v)
 kill -9 `ps | grep mentohust| grep -v grep|awk '{print $1}'`
 echo -e "${RED}已关闭打开的mentohust${RES}"
 fi
 flag=1
 OLD_IFS=$IFS   
 IFS=$'\n'        
-for line in $(cat $CONF_PATH)
+for line in $(cat ${CONF_PATH})
 do
 echo -e "${GREEN}----正在开启第${flag}个mentohust----${RES}"
 sleep 1s
