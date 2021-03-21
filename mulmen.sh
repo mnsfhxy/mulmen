29684890c6420020968a99fadc3cff15284bc1bf
mnsfhxy <mnsfhxy@163.com>
2021-03-21 13:54:37 +0800

    two

--- mulmen.sh
+++ mulmen.sh
@@ -4,7 +4,16 @@ GREEN='\E[1;32m'
 PINK='\E[1;35m'     
 RES='\E[0m'          
 PID_PATH='/var/mentohust.pid' #mentohust.pid的路径
+CONF_PATH='/etc/mulmen.conf' #配置文件路径
 MENTOHUST_STATU=$(ps -ef |grep -w mentohust|grep -v grep|wc -l)
+if [ -d $CONF_PATH ]
+then
+echo -e ${RED}加载配置文件${CONF_PATH}${RES}
+else
+echo -e ${RED}${CONF_PATH}配置文件不存在!!${RES}
+exit
+fi
+echo -e ${RED}${RES}
 if [ ${MENTOHUST_STATU} -gt 0 ]
 then
 echo -e "${RED}检测到已开启mentohust${RES}"
@@ -15,7 +24,7 @@ fi
 flag=1
 OLD_IFS=$IFS   
 IFS=$'\n'        
-for line in $(cat mulmen.conf)
+for line in $(cat $CONF_PATH)
 do
 echo -e "${GREEN}----正在开启第${flag}个mentohust----${RES}"
 sleep 1s
